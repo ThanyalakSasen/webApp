31316ff53c5806843b0535480bbdff996769473e
ThanyalakSasen <167286615+ThanyalakSasen@users.noreply.github.com>
2024-09-23 04:49:17 +0700

    Initial commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,56 @@
+<x-guest-layout>
+    <x-authentication-card>
+        <x-slot name="logo">
+            <x-authentication-card-logo />
+        </x-slot>
+
+        <x-validation-errors class="mb-4" />
+
+        @session('status')
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ $value }}
+            </div>
+        @endsession
+
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="pt-10 pr-15 pb-10 pl-5">
+                <div>
+                    <x-label for="email" value="{{ __('Email') }}" />
+                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                </div>
+    
+                <div class="mt-5">
+                    <x-label for="password" value="{{ __('Password') }}" />
+                    <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                </div>
+                <div class="flex items-center justify-end mt-1">
+                    @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                    @endif
+                </div>
+                <div class="flex items-center justify-start mt-1">
+                    <label for="remember_me" class="flex items-center justify-start">
+                        <x-checkbox id="remember_me" name="remember" />
+                        <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    </label>
+                </div>
+    
+                {{-- <div class="flex items-center mt-10">
+                    <label for="remember_me" class="flex items-center justify-start">
+                        <x-checkbox id="remember_me" name="remember" />
+                        <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    </label>
+                </div> --}}
+                
+                <div class="flex items-center justify-center mt-4">
+                <x-button class="ms-4">
+                    {{ __('Log in') }}
+                </x-button>
+                </div>
+            </div>
+        </form>
+    </x-authentication-card>
+</x-guest-layout>
