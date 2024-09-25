511bde12e2760a807153e2a70f2d42333d88d6a5
ThanyalakSasen <167286615+ThanyalakSasen@users.noreply.github.com>
2024-09-26 00:09:43 +0700

    updatelast

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,21 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/parking', [ParkingController::class, 'showForm'])->name('showForm');
-    Route::post('/parking', [ParkingController::class, 'submitForm'])->name('submitForm');
-});
-
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
 });
+
+
+// Route::middleware([
+//     'admin',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
+
+
+    Route::get('/parking', [ParkingController::class, 'showForm'])->name('showForm');
+    Route::post('/parking', [ParkingController::class, 'submitForm'])->name('submitForm');
+
